52dffdc7a8db024c5ef166764ae6922e8069acc8
dawood sher jan <alpha72784@outlook.com>
2025-03-14 19:32:42 +0100

    implemented 2 instructions

--- CHIP8_emulator/utility.cpp
+++ CHIP8_emulator/utility.cpp
@@ -34,12 +34,31 @@ void disassember(std::vector<uint8_t> hex_data, int pc)
 	std::cout << std::format("\n{:#06x}  {:#04x}  {:#04x}", (pc), hex_value, hex_data[pc+1]);
 	std::cout << "\nFirst Nibble: " << std::format("{:#01x}\n", firstnib);
 
+
 	switch (firstnib)
 	{
 	case 0x00: std::cout << "\nclear the display"; break;
-	case 0x01: std::cout << "\nN/A "; break;
-	case 0x02: std::cout << "\nN/A "; break;
-	case 0x03: std::cout << "\nN/A "; break;
+	case 0x01: 
+	{
+		// 1NNN: jump to instruction NNN
+		uint16_t nnn = ((hex_value << 4) << 8) | (hex_data[pc + 1]<<4);
+		// TODO: add 0x200 to the pc
+		pc = nnn;
+		std::cout << "\nnnn: " << std::format("{:#04x}\n", nnn);
+		break;
+	}
+	case 0x02: 
+	{
+		// 2NNN: Execute subroutine starting at address NNN
+		uint16_t nnn = ((hex_value << 4) << 8) | (hex_data[pc + 1]<<4);
+		std::cout << "\nnnn: " << std::format("{:#04x}\n", nnn);
+		break;
+	}
+	case 0x03: 
+	{
+		// 3XNN: Skip the following instruction if the value of register VX equals NN
+		break;
+	}
 	case 0x04: std::cout << "\nN/A "; break;
 	case 0x05: std::cout << "\nN/A "; break;
 	case 0x06: std::cout << "\nN/A "; break;
@@ -70,7 +89,8 @@ void traverse_rom(std::vector<uint8_t> hex_data)
 	{
 		disassember(hex_data, i);
 		i += 2;
-		break;
+		//if (i > 10)
+			break;
 	}
 }
 
