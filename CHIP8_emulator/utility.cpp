8c46f40a5ab454965f635aa1bc022971002d2ce5
dawood sher jan <alpha72784@outlook.com>
2025-03-14 21:54:21 +0100

    added a few more instructions. using reference for vectors.

--- CHIP8_emulator/utility.cpp
+++ CHIP8_emulator/utility.cpp
@@ -22,7 +22,7 @@ std::vector<uint8_t> read_hex_file(const char* filepath)
 	return hex_data;
 }
 
-void disassember(std::vector<uint8_t> hex_data, int pc)
+void disassember(std::vector<uint8_t> &hex_data, int& pc)
 {
 	if (pc >= hex_data.size()-1) 
 		return;
@@ -52,16 +52,41 @@ void disassember(std::vector<uint8_t> hex_data, int pc)
 		// 2NNN: Execute subroutine starting at address NNN
 		uint16_t nnn = ((hex_value << 4) << 8) | (hex_data[pc + 1]<<4);
 		std::cout << "\nnnn: " << std::format("{:#04x}\n", nnn);
+		// TODO: some stack operations required
 		break;
 	}
 	case 0x03: 
 	{
 		// 3XNN: Skip the following instruction if the value of register VX equals NN
+		uint8_t register_x{};	// in the array of registers search for secondnib
+		if (register_x == hex_data[pc + 1])
+			pc += 2;
 		break;
 	}
-	case 0x04: std::cout << "\nN/A "; break;
-	case 0x05: std::cout << "\nN/A "; break;
-	case 0x06: std::cout << "\nN/A "; break;
+	case 0x04: 
+	{
+		// 4XNN	Skip the following instruction if the value of register VX is not equal to NN
+		uint8_t register_x{};  // in the array of registers search for secondnib
+		if (register_x != hex_data[pc + 1])
+			pc += 2;
+		break;
+	}
+	case 0x05: 
+	{
+		// 5XY0	Skip the following instruction if the value of register VX is equal to the value of register VY
+		uint8_t register_x{};  // get the values of the resgister using the second nibble
+		uint8_t register_y{};
+		if (register_x == register_y)
+			pc += 2;
+		break;
+	}
+	case 0x06: 
+	{
+		// 6XNN	Store number NN in register VX
+		// Pseudo: 
+		// registers[secondnib] = hex_data[pc+1]
+		break;
+	}
 	case 0x07: std::cout << "\nN/A "; break;
 	case 0x08: std::cout << "\nN/A "; break;
 	case 0x09: std::cout << "\nN/A "; break;
@@ -80,7 +105,7 @@ void disassember(std::vector<uint8_t> hex_data, int pc)
 
 }
 
-void traverse_rom(std::vector<uint8_t> hex_data)
+void traverse_rom(std::vector<uint8_t>& hex_data)
 {
 	// FIXME: only for debug. normally, i should start from 0.
 	//int i = 0;
--- CHIP8_emulator/utility.h
+++ CHIP8_emulator/utility.h
@@ -3,5 +3,5 @@
 #include <vector>
 
 std::vector<uint8_t> read_hex_file(const char*);
-void disassember(std::vector<uint8_t>, int);
-void traverse_rom(std::vector<uint8_t>);
+void disassember(std::vector<uint8_t>&, int&);
+void traverse_rom(std::vector<uint8_t>&);
